347b12db65afd9dde367eaed0461b070147e1d0e
u_mulder <m264695502@gmail.com>
2017-01-07 12:13:44 +0300

    - Add more tests  - Lint codes

--- codegen_test.go
+++ codegen_test.go
@@ -1,8 +1,8 @@
 package codegen_test
 
 import (
-    "fmt"
-    "github.com/u-mulder/codegen"
+	"fmt"
+	"github.com/u-mulder/codegen"
 )
 
 //func ExampleFoo()     // documents the Foo function or type
@@ -10,33 +10,64 @@ import (
 //func Example()        // documents the package as a whole
 
 func ExampleAddGetExistingSnippet() {
-    c, _ := codegen.New()
+	c, _ := codegen.New()
 
-    c.AddSnippet("SnippetNo1", "SomeCodeGoesHere")
+	c.AddSnippet("SnippetNo1", "SomeCodeGoesHere")
 
-    fmt.Println(c.GetSnippet("SnippetNo1"))
-    // Output: SomeCodeGoesHere <nil>
+	fmt.Println(c.GetSnippet("SnippetNo1"))
+	// Output: SomeCodeGoesHere <nil>
 }
 
 func ExampleAddGetNonExistingSnippet() {
-    c, _ := codegen.New()
+	c, _ := codegen.New()
 
-    c.AddSnippet("SnippetNo1", "SomeCodeGoesHere")
+	c.AddSnippet("SnippetNo1", "SomeCodeGoesHere")
 
-    fmt.Println(c.GetSnippet("SnippetNo2"))
-    // Output: Snippet with such key not found
+	fmt.Println(c.GetSnippet("SnippetNo2"))
+	// Output: Snippet with such key not found
 }
 
 func ExampleEncloseInSingleQuotes() {
-    fmt.Println(codegen.EncloseInSingleQuotes("VALUE"))
-    // Output: 'VALUE'
+	fmt.Println(codegen.EncloseInSingleQuotes("VALUE"))
+	// Output: 'VALUE'
 }
 
 func ExampleCodegen_AddDefaultSnippets() {
-    c, _ := codegen.New()
+	c, _ := codegen.New()
 
-    c.AddDefaultSnippets()
+	c.AddDefaultSnippets()
 
-    fmt.Println(c.GetSnippet("mevent_obj"))
-    // Output: $meo = new CEventType; <nil>
+	fmt.Println(c.GetSnippet("mevent_obj"))
+	// Output: $meo = new CEventType; <nil>
+}
+
+func ExampleCodegen_Generate() {
+	c, _ := codegen.New()
+
+	c.AddSnippet("php_quick_echo", "<?=")
+	c.AddSnippet("php_closing_tag", "?>")
+
+	c.RegisterGenerator("qe_script", quickEcho)
+	r, _ := c.Generate("qe_script")
+
+	fmt.Println(r)
+	// Output: <?='123-321'?>
+}
+
+func quickEcho(c *codegen.Codegen) string {
+	var r, tmpStr string
+
+	tmpStr, _ = c.GetSnippet("php_quick_echo")
+	if "" != tmpStr {
+		r = tmpStr
+	}
+
+	r += codegen.EncloseInSingleQuotes("123-321")
+
+	tmpStr, _ = c.GetSnippet("php_closing_tag")
+	if "" != tmpStr {
+		r += tmpStr
+	}
+
+	return r
 }
